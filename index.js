6a16227c174ba6f474d8bb18439b9a04d0e572e5
Anders Hornor <anders.hornor@gmail.com>
2018-11-28 22:16:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {key: 'value'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({[key]: value}, object);
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
